3f080b228f9680f7ccf5d058d867bfb88fcd6651
Ivan Scrobot <ivanscrobot@gmail.com>
2023-04-12 15:28:25 -0700

    first version of service gateway is ready

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 from my_auth import validate
 from auth_svc import access
-from storage import util
+from my_storage import util
 
 server = Flask(__name__)
 server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
@@ -46,3 +46,6 @@ def upload():
 @server.route("/download", methods=["GET"])
 def download():
     pass
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=8080)
